7ef6fbcf771dac4ac98bb14e6e5d1d21bf04c05e
neemias brito <neeemiasaimeeen@gmailcom>
2022-10-23 10:26:34 -0300

    projeto de site

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+
+
+
+var listaFilmes = [
+  "https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg",
+  "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
+  "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/90/98/20169244.jpg",
+  "https://capas-p.imagemfilmes.com.br/164805_000_p.jpg",
+  "https://capas-p.imagemfilmes.com.br/164947_000_p.jpg",
+  "https://capas-p.imagemfilmes.com.br/164039_000_p.jpg",
+  "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
